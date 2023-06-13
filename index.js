bb3ba95faaa125ef6989ea4d97e658e0075985a5
MuhammadAtiq <muhammadatiq757@gamil.com>
2023-06-14 02:12:40 +0600

    class post done

--- index.js
+++ index.js
@@ -60,26 +60,26 @@ async function run() {
 
 
 
-    // app.post('/users', async (req, res) => {
-    //   const user = req.body;
-    //   const query = { email: user.email }
-    //   const existingUser = await userCollection.findOne(query);
-    //   if (existingUser) {
-    //     return res.send({ message: 'user already exists' })
-    //   }
-    //   const result = await userCollection.insertOne(user);
-    //   res.send(result)
-
-    // })
-
-
-
-    app.post('/addclass', async (req, res) => {
-      const adding = req.body;
-      const result = await addingCollection.insertOne(adding)
+    app.post('/users', async (req, res) => {
+      const user = req.body;
+      const query = { email: user.email }
+      const existingUser = await userCollection.findOne(query);
+      if (existingUser) {
+        return res.send({ message: 'user already exists' })
+      }
+      const result = await userCollection.insertOne(user);
       res.send(result)
-      // console.log(adding);
+
     })
+
+
+
+    // app.post('/addclass', async (req, res) => {
+    //   const adding = req.body;
+    //   const result = await addingCollection.insertOne(adding)
+    //   res.send(result)
+    //   // console.log(adding);
+    // })
 
 
     app.get('/selectedClassData', async (req, res) => {
